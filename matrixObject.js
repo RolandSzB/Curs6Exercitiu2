9a72c6980716c6b581430c98c5845b83ecbaea1a
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2023-04-21 11:05:01 +0300

    matrix and colors randomized

--- matrixObject.js
+++ matrixObject.js
@@ -10,13 +10,40 @@ const m2 = [
   [{ value: 3 }, { value: 2 }, { value: 1 }],
 ];
 
+//facem matrixul dinamic
+function creareMatrix(linii, coloane) {
+  let mat = [];
+  for (let i = 0; i < linii; i++) {
+    mat.push([]);
+    for (let j = 0; j < coloane; j++) {
+      mat[i].push({
+        value: Math.floor(Math.random() * 9) + 1,
+        color:
+          "rgb(" +
+          Math.floor(Math.random() * 255) +
+          "," +
+          Math.floor(Math.random() * 255) +
+          "," +
+          Math.floor(Math.random() * 255) +
+          ")",
+      });
+    }
+  }
+  console.log(mat);
+  return mat;
+}
+
 function sumMatrixObj(m1, m2) {
   let ms = []; //ms=matrix-suma
   for (let i = 0; i < m1.length; i++) {
     ms.push([]);
     for (let j = 0; j < m1[i].length; j++)
-      ms[i].push({ value: m1[i][j].value + m2[i][j].value });
+      ms[i].push({
+        value: m1[i][j].value + m2[i][j].value,
+        color: "red",
+      });
   }
   return ms;
 }
 console.log(sumMatrixObj(m1, m2));
+console.log(creareMatrix(3, 3), creareMatrix(3, 3));
